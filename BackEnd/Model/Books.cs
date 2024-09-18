f73808b75a51d5a57450d19856576dea73b4888c
EDWIN RODRIGO PEDRAZA QUINTERO <erpedraza@ucundinamarca.edu.co>
2024-09-17 19:04:15 -0500

    hhh

--- BackEnd/Model/Books.cs
+++ BackEnd/Model/Books.cs
@@ -1,6 +1,6 @@
 ﻿namespace BackEnd.Model
 {
-    public class Books
+    public class Books///gg
     {
         public int Id { get; set; }
         public virtual required Edition Edition { get; set; }
