e4e35143f084ed3f0b32f9ccbc2671f14718152a
LUIS ALEJANDRO CHABUR GUEVARA <lchabur@ucundinamarca.edu.co>
2024-09-29 01:24:16 -0500

    Cambios en el repositorio, quedan funcionando, se revisa el put para lo que seria books

--- BackEnd/Services/BooksService.cs
+++ BackEnd/Services/BooksService.cs
@@ -1,5 +1,6 @@
 ﻿using BackEnd.Model;
 using BackEnd.Repository;
+using BackEnd.Context; // Asegúrate de que esta línea esté aquí y no dentro de un bloque
 
 namespace BackEnd.Services
 {
@@ -47,4 +48,3 @@ namespace BackEnd.Services
         }
     }
 }
-
